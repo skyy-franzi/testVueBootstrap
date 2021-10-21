aeea9859497d486c638173ef25f675726c80128f
skyy-franzi <56691064+skyy-franzi@users.noreply.github.com>
2021-10-21 15:30:00 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="text-center">
+      Bootstrap-Vue Skeleton
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+
+</style>
